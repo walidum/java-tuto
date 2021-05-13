c075d096cfe282e266cf0e79266194dd46fdd0f1
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-05-13 09:52:32 +0100

    Detect Pangram

--- java-collections/src/MainCollections.java
+++ java-collections/src/MainCollections.java
@@ -1,12 +1,22 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class MainCollections {
     public static void main(String[] args) {
         Logger log = Logger.getLogger(MainCollections.class.getName());
+
+        HashMap<Integer, String> hashmap = new HashMap<>();
+        hashmap.put(2, "dredte");
+        hashmap.put(2, "dddffdf");
+        hashmap.entrySet().stream().forEach(e -> log.log(Level.INFO, "" + e.getKey() + "------" + e.getValue()));
+        Iterator<Map.Entry<Integer, String>> it = hashmap.entrySet().iterator();
+        while (it.hasNext()) {
+            log.log(Level.INFO, it.next().getValue());
+        }
+    }
+
+    private static void EterateList(Iterator it, Logger log) {
 
         LinkedList<String> linkedList = new LinkedList<>();
         linkedList.addFirst("first");
@@ -19,9 +29,6 @@ public class MainCollections {
         arrayList.add("third");
         arrayList.remove(arrayList.get(0));
         arrayList.stream().forEach((e) -> log.log(Level.INFO, e));
-    }
-
-    private static void EterateList(Iterator it, Logger log) {
         while (it.hasNext()) {
             log.log(Level.INFO, it.next().toString());
         }
